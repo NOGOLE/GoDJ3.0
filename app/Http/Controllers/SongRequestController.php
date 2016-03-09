70afdfb1f350ec0a2cd6e0b5933cd00c3c9c93a5
Jyrone Parker <jyrone.parker@gmail.com>
2016-03-09 23:22:12 +0000

    Added SongRequestController. Added relationships

---
+++ app/Http/Controllers/SongRequestController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\SongRequest;
+use App\Http\Requests;
+
+class SongRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $user = \App\User::where('name', $request->user_id);
+        $songRequest = SongRequest::Create([
+          'user_id' => $user->id,
+          'title' => $request->title,
+          'artist' => $request->artist,
+          'lat' => $request->lat,
+          'long'=> $request->long,
+          ]);
+        if($songRequest->isValid()){
+          return response()->json([
+            'success' => true
+            ]);
+        }
+        else{
+          return response()->json([
+            'success' => false
+            ]);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/SongRequest.php
+++ app/SongRequest.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class SongRequest extends Model
 {
     //
+    protected $fillable = [
+      'user_id',
+      'title',
+      'artist',
+      'lat',
+      'long',
+    ];
+
+    public function user(){
+      return $this->belongsTo('App\User');
+    }
 }
