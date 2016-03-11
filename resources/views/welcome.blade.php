8f7d447d523f6db488dfba5768475f04c0bcb546
Jyrone Parker <jyrone.parker@gmail.com>
2016-03-11 05:11:18 -0800

    Styling on front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
-  <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-          <div class="panel panel-default">
-              <div class="panel-heading">Real-Time Requests</div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Real-Time Requests</div>
 
-              <div class="panel-body">
-                Songs
-                  <canvas id="songs" width="500" height="400"></canvas>
-                Moods
-                  <canvas id="moods" width="500" height="400"></canvas>
+                <div class="panel-body">
+                  <div class="row">
+                    <div class="col-md-6">
+                    <canvas id="songs" width="350" height="400"></canvas>
+                    <hr>
+                    <h2>Most Requested Song: <b id="most-requested-song"></b></h2>
+                  </div>
+                  <div class="col-md-6">
+                    <canvas id="moods" width="350" height="400"></canvas>
+                    <hr>
+                    <h2>Most Requested Mood: <b id="most-requested-mood"></b></h2>
+                  </div>
+                </div>
 
-              </div>
-          </div>
-      </div>
-  </div>
-  <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-          <div class="panel panel-default">
-              <div class="panel-heading">Real-Time Map</div>
-              <style>
-              #map {
-                height: 100%;
-              }
-              </style>
-              <div class="panel-body">
-                  <div id="map"></div>
-              </div>
-          </div>
-      </div>
-  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
@@ -78,11 +73,11 @@ function getLocation() {
 }
 function showPosition(position) {
 
- document.getElementById('song_lat').value = position.coords.latitude;
- document.getElementById('mood_lat').value = position.coords.latitude;
+ $('#song_lat').val() = position.coords.latitude;
+ $('#mood_lat').val() = position.coords.latitude;
 
- document.getElementById('song_long').value = position.coords.longitude;
-document.getElementById('mood_long').value = position.coords.longitude;
+ $('#song_long').val() = position.coords.longitude;
+$('#mood_long').val() = position.coords.longitude;
 }
 window.onload=getLocation();
 </script>
@@ -139,7 +134,6 @@ function updateSongData(mySongChart, data, songs){
   mySongChart.segments[$.inArray(data.songRequest.title, songs)].value += 1;
   mySongChart.update();
   songs.push(data.songRequest.title);
-  updateSongMarkers(data);
   updateMostRequestedSongs(mySongChart.segments);
 }
 
@@ -179,7 +173,7 @@ color: color,
 highlight: highlight,
 label: data.moodRequest.title
 });
-updateMoodMarkers(data);
+
 updateMostRequestedMoods(myMoodChart.segments);
 }
 
@@ -203,50 +197,7 @@ function updateMostRequestedMoods(moods){
   document.getElementById("most-requested-mood").innerHTML = moods[index].label;
 }
 </script>
-<script>
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 41.850033, lng: -87.6500523},
-          zoom: 4
-        });
-      }
-      function updateSongMarkers(data){
-        var pinIcon = new google.maps.MarkerImage(
-    'http://www.clker.com/cliparts/e/3/F/I/0/A/google-maps-marker-for-residencelamontagne-hi.png',
-    null, /* size is determined at runtime */
-    null, /* origin is 0,0 */
-    null, /* anchor is bottom center of the scaled image */
-    new google.maps.Size(42, 68)
-);
-        var marker = new google.maps.Marker({
-      position: {lat: Number(data.songRequest.lat), lng: Number(data.songRequest.long)},
-      map: map,
-      animation: google.maps.Animation.DROP,
-      icon:pinIcon,
-      title: data.songRequest.title + ' by  ' + data.songRequest.artist
-    });
-      }
 
-      function updateMoodMarkers(data){
-        var pinIcon = new google.maps.MarkerImage(
-    'http://www.clker.com/cliparts/8/6/U/z/k/o/google-maps-marker-for-residencelamontagne-hi.png',
-    null, /* size is determined at runtime */
-    null, /* origin is 0,0 */
-    null, /* anchor is bottom center of the scaled image */
-    new google.maps.Size(42, 68)
-);
-        var marker = new google.maps.Marker({
-      position: {lat: Number(data.moodRequest.lat), lng: Number(data.moodRequest.long)},
-      map: map,
-      icon:pinIcon ,
-      animation: google.maps.Animation.DROP,
-      title: data.moodRequest.title
-    });
-      }
-    </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCDNt1biVyfA8h-eCZyZ69CKS6NNBCeEQ&callback=initMap"
-    async defer></script>
     <script>
     function sendSong(){
       var data= {
