c4a2364a9a7e27b05baefe7ceb83c33be95a97b3
Jyrone Parker <jyrone.parker@gmail.com>
2016-06-11 08:14:55 -0400

    Try to do socket over ssl

--- node/server.js
+++ node/server.js
@@ -1,31 +1,57 @@
+var fs = require('fs');
 
-var app = require('http').createServer(handler);
+var pkey = fs.readFileSync('/etc/nginx/ssl/godj.online/98595/server.key');
+
+var pcert = fs.readFileSync('/etc/nginx/ssl/godj.online/98595/server.crt')
+
+var options = {
+
+  key: pkey,
+
+  cert: pcert
+
+};
+
+var app = require('https').createServer(options);
+
 var io = require('socket.io')(app);
 
 var Redis = require('ioredis');
+
 var redis = new Redis();
 
-app.listen(3000, function() {
+app.listen(9000, function() {
+
     console.log('Server is running!');
+
 });
-console.log(app);
 
 function handler(req, res) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
     res.writeHead(200);
+
     res.end('');
+
 }
 
 io.on('connection', function(socket) {
+
     //
-    console.log('New Connection: ' + socket);
+
 });
 
 redis.psubscribe('*', function(err, count) {
+
     //
+
 });
 
 redis.on('pmessage', function(subscribed, channel, message) {
-  console.log(message);
+
     message = JSON.parse(message);
-    io.emit(channel, message);
-});
+
+    console.log('Channel is ' + channel + ' and message is ' + message);
+
+    io.emit(channel, message.data);
