fd76f136eec1fc9478f163d37a9140bf6f3b4fe5
Jyrone Parker <jyrone.parker@gmail.com>
2016-06-11 08:46:01 -0400

    Trying something

--- resources/views/scripts/socket.blade.php
+++ resources/views/scripts/socket.blade.php
@@ -2,7 +2,7 @@
 <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="bower_components/Chart.js/Chart.js"></script>
-<script src="{{env('APP_URL')}}:3000/socket.io/socket.io.js"></script>
+<script src="{{env('APP_URL')}}/socket.io/socket.io.js"></script>
 <script>
 var songData = [];
 var moodData = [];
